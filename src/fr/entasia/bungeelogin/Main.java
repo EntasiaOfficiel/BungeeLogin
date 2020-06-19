585bfeb244bb6e4a82d06be0e0e3a5eefbf1283f
iTrooz_ <team.mortelgames@gmail.com>
2020-06-19 18:58:25 +0200

     

--- src/fr/entasia/bungeelogin/Main.java
+++ src/fr/entasia/bungeelogin/Main.java
@@ -6,6 +6,11 @@ import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.api.plugin.PluginDescription;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Set;
 
 public class Main extends Plugin {
 
